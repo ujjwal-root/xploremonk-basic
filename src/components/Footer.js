e4a17f2e358b1b665346ca5472dd49b7f509d12e
Your ujjwal-root <anandujjwalbns@gmail.com>
2024-07-06 17:58:36 +0530

    fixing security issues for deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,28 @@ const Footer = () => {
         <footer>
             <div className="container-footer">
                 <ul className="social-btns">
-                <li className="social-btn">
-                    <a href="https://instagram.com/thexploremonk" target="_blank"><i className="fab fa-instagram" ></i></a>
-                </li>
-                <li className="social-btn">
-                    <a href="https://www.linkedin.com/in/ujjwal-tfd" target="_blank"><i className="fab fa-linkedin-in" ></i></a>
-                </li>
-                <li className="social-btn">
-                    <a href="https://github.com/ujjwal-root" target="_blank"><i className="fab fa-github" ></i></a>
-                </li>
+                    <li className="social-btn">
+                        <a href="https://instagram.com/thexploremonk" target="_blank" rel="noreferrer"><i className="fab fa-instagram" ></i></a>
+                    </li>
+                    <li className="social-btn">
+                        <a href="https://www.linkedin.com/in/ujjwal-tfd" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in" ></i></a>
+                    </li>
+                    <li className="social-btn">
+                        <a href="https://github.com/ujjwal-root" target="_blank" rel="noreferrer"><i className="fab fa-github" ></i></a>
+                    </li>
                 </ul>
 
                 <div className="to-top">
-                <a className="rocket" href="#root">
-                    <img src="/travel.png" alt="ico" /> <br />
-                </a>
+                    <a className="rocket" href="#root">
+                        <img src="/travel.png" alt="ico" /> <br />
+                    </a>
                 </div>
 
                 <p className="copyright">
-                <i className="far fa-copyright" aria-hidden="true"></i> Xploremonk {new Date().getFullYear()}
+                    <i className="far fa-copyright" aria-hidden="true"></i> Xploremonk {new Date().getFullYear()}
                 </p>
             </div>
-            </footer>
+        </footer>
     )
 }
 
